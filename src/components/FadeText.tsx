9e6b760f1f5890bde0ca4bac31577282e34c46ed
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-07-02 20:45:29 +0530

    fix: js to tsx

--- src/components/FadeText.tsx
+++ src/components/FadeText.tsx
@@ -2,8 +2,12 @@
 import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
-const FadeText = ({ children }) => {
-  const textRef = useRef(null);
+interface FadeTextProps {
+  children: React.ReactNode;
+}
+
+const FadeText: React.FC<FadeTextProps> = ({ children }) => {
+  const textRef = useRef<HTMLSpanElement>(null);
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -13,19 +17,23 @@ const FadeText = ({ children }) => {
         setIsVisible(entry.isIntersecting);
       },
       {
-        threshold: 0.2, 
+        threshold: 0.1,
       }
     );
 
-    observer.observe(textRef.current);
+    if (textRef.current) {
+      observer.observe(textRef.current);
+    }
 
     return () => {
-      
+      if (textRef.current) {
+        observer.unobserve(textRef.current);
+      }
     };
   }, []);
 
   useEffect(() => {
-    if (isVisible) {
+    if (isVisible && textRef.current) {
       const lines = Array.from(textRef.current.children);
 
       const tl = gsap.timeline({ defaults: { ease: 'power3.out' } });
@@ -35,8 +43,6 @@ const FadeText = ({ children }) => {
         duration: 1.5,
         y: '0%',
         opacity: 1,
-        
-        
       });
     }
   }, [isVisible]);
@@ -51,3 +57,4 @@ const FadeText = ({ children }) => {
 };
 
 export default FadeText;
+
---
+++ src/components/RevealText.tsx
@@ -0,0 +1,54 @@
+
+"use client"
+import React, { useEffect, useRef, useState } from 'react';
+
+import { gsap } from 'gsap';
+
+interface TextRevealProps {
+  children: React.ReactNode;
+}
+
+const TextReveal: React.FC<TextRevealProps> = ({ children }) => {
+  const textRef = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries;
+      setIsVisible(entry.isIntersecting);
+    });
+
+    if (textRef.current) {
+      observer.observe(textRef.current);
+    }
+
+    return () => {
+      if (textRef.current) {
+        observer.unobserve(textRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible && textRef.current) {
+      const lines = Array.from(textRef.current.children) as HTMLDivElement[];
+
+      gsap.from(lines, {
+        opacity: 0,
+        x: 150,
+        duration: 1.6,
+        delay: 0.6,
+        stagger: 0.3,
+        ease: 'power3.out',
+      });
+    }
+  }, [isVisible]);
+
+  return (
+    <div ref={textRef} className="h-full w-full">
+      {children}
+    </div>
+  );
+};
+
+export default TextReveal;
