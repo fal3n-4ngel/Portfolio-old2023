1a336711062cd00584f0038a1c44d971f9b567bf
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-07-03 09:43:23 +0530

    update : responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="items-center justify-center overflow-x-hidden">
       <nav className="fixed w-full  z-10 flex justify-between py-14 px-[18%] text-xl ">
         <div className="hover:cursor-pointer">Adi</div>
-        <div className="flex gap-[40%]">
+        <div className="flex gap-[40%] invisible md:visible">
           <a className="hover:cursor-pointer" href="#">
             home
           </a>
@@ -25,16 +25,16 @@ export default function Home() {
             linkedin
           </a>
         </div>
-        <a href="mailto:adiadithyakrishnan@gmail.com">contact</a>
+        <a href="mailto:adiadithyakrishnan@gmail.com" className="invisible md:visible">contact</a>
       </nav>
-      <div className="h-full hidden md:flex font-light text-4xl md:text-7xl leading-[5rem] tracking-[0px] w-full px-[18%] py-[20%] items-center">
+      <div className="h-full  hidden  md:flex font-light text-4xl md:text-7xl leading-[5rem] tracking-[0px] w-full px-[18%] py-[20%] items-center">
         <RevealText>
-          <div> I&apos;m Adi - a Multidisciplinary </div>
+          <div > I&apos;m Adi - a Multidisciplinary </div>
           <div> Developer with a focus on Web, </div>
           <div> App dev and Scripting </div>
         </RevealText>
       </div>
-      <div className="h-full flex md:hidden font-light text-4xl md:text-7xl leading-[5rem] tracking-[0px] w-full px-[18%] py-[20%] items-center">
+      <div className="h-full mt-16 flex md:hidden font-light text-4xl md:text-7xl leading-[5rem] tracking-[0px] w-full px-[18%] py-[20%] items-center">
         <RevealText>
           <div>
             I&apos;m Adi - a Multidisciplinary Developer with a focus on Web,
@@ -45,23 +45,23 @@ export default function Home() {
 
       <div className="flex min-h-screen font-light leading-relaxed pointer-events-none">
         <>
-          <div className="flex md:flex-row flex-col p-2 md:p-0 md:w-[75%] mx-auto ">
+          <div className="flex md:flex-row md:text-4xl text-3xl flex-col p-2 md:p-0 md:w-[75%] mx-auto ">
             <div className="flex flex-col md:w-[50%]">
               <FadeText>
-                <div className=" text-4xl m-10 ease-in ">
+                <div className="  m-10 ease-in ">
                   With a passion for bringing ideas to life, I thrive on solving
                   puzzles, fixing bugs, tackling mind-boggling problems.
                 </div>
               </FadeText>
               <FadeText>
-                <div className="text-4xl m-10 ease-in ">
+                <div className=" m-10 ease-in ">
                   While I enjoy exploring web and app development for fun, my
                   true expertise lies in crafting exceptional work arounds for
                   challenging problems.
                 </div>
               </FadeText>
               <FadeText>
-                <div className=" text-4xl m-10 ease-in ">
+                <div className="  m-10 ease-in ">
                   I spent my free time either &lsquo;watching or reading or
                   coding&rsquo; stupid stuffs.
                 </div>
@@ -126,13 +126,13 @@ export default function Home() {
         </>
       </div>
 
-      <div className="min-h-screen font-light text-4xl md:w-[75%] mx-auto">
+      <div className="min-h-screen font-light text-4xl md:w-[75%] p-10 md:p-0 mx-auto">
         <FadeText><div className="text-6xl">Projects</div></FadeText>
         <FadeText>
           <Projcard
-            url1="drishti.png"
-            url2="drishti1.png"
-            url3="drishti2.png"
+            url1="/drishti.png"
+            url2="/drishti1.png"
+            url3="/drishti2.png"
             name="Drishti Client"
             description="
             Drishti client is a client side flutter application
@@ -147,39 +147,26 @@ export default function Home() {
         </FadeText>
         <FadeText>
           <Projcard
-            url1="drishti.png"
-            url2="drishti1.png"
-            url3="drishti2.png"
-            name="Drishti Client"
+            url1="/sus.png"
+            url2="/sus1.png"
+            url3="/sus2.png"
+            name="Sus Goals"
             description="
-            Drishti client is a client side flutter application
-            for Drishti that will be available to public through 
-            which they can report and get updates about accidents 
-            and other notable incidents in the locailty
+            Sus Goals is simply a project for my Design and Engineering Course under BTech in CSE :<
+            Anyway this site gives you a random Sustainable Goal that refreshes daily,DO IT
+             and click the done button, it will increment the world wide counter for the goals reached. 
+            
             "
-            type="App (Android,Ios)"
-            event="Define Hackathon 2022"
-            view="https://github.com/fal3n-4ngel/Drishti-Client"
+            type="Website"
+            event="Collge Project"
+            view="https://github.com/fal3n-4ngel/SusGoals"
           />
         </FadeText>
-        <FadeText>
-          <Projcard
-            url1="drishti.png"
-            url2="drishti1.png"
-            url3="drishti2.png"
-            name="Drishti Client"
-            description="
-            Drishti client is a client side flutter application
-            for Drishti that will be available to public through 
-            which they can report and get updates about accidents 
-            and other notable incidents in the locailty
-            "
-            type="App (Android,Ios)"
-            event="Define Hackathon 2022"
-            view="https://github.com/fal3n-4ngel/Drishti-Client"
-          />
-        </FadeText>
+       
+        
+        
       </div>
+      <div className="flex justify-center text-3xl my-[100px] font-thin text-slate-300">:&gt; Adithya Krishnan 2023</div>
     </main>
   );
 }
--- src/components/Projcard.tsx
+++ src/components/Projcard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 import Link from "next/link";
-
+import Image from "next/image";
 type projProps = {
   url1: string;
   url2: string;
@@ -16,8 +16,10 @@ type projProps = {
 };
 
 function Projcard(props: projProps) {
+  
   return (
-    <div className="flex flex-col mt-10 ">
+    
+    <div className="flex flex-col mt-10 w-full">
       <div className="text-5xl my-8">{props.name}</div>
       <Splide
         options={{
@@ -33,42 +35,57 @@ function Projcard(props: projProps) {
         }} 
       >
         <SplideSlide className="delay-1000 pointer-events-none">
-          <img
+          <Image
             src={props.url1}
             alt={props.name}
-            className="w-full object-cover overflow-hidden"
+            className="w-full object-cover overflow-hidden  md:h-[95%]"
+            width={1980}
+            height={900}
           />
         </SplideSlide>
         <SplideSlide>
-          <img
+        <Image
             src={props.url2}
             alt={props.name}
-            className="w-full object-cover overflow-hidden"
+            className="w-full object-cover overflow-hidden  md:h-[95%]"
+            width={1980}
+            height={900}
+            
           />
         </SplideSlide>
         <SplideSlide>
-          <img
+        <Image
             src={props.url3}
             alt={props.name}
-            className="w-full  object-cover overflow-hidden"
+            className="w-full object-cover overflow-hidden  md:h-[95%]"
+            width={1980}
+            height={900}
           />
         </SplideSlide>
       </Splide>
-      <div className="flex mt-8">
+      <div className="flex md:flex-row flex-col mt-8">
         <div className="flex flex-col w-full  md:pr-[5%] text-2xl ">
           <div><span className="text-gray-400">Type : </span>{props.type}</div>
           <div><span className="text-gray-400">Event : </span>{props.event}</div>
           <div className=" flex justify-center h-full items-center ">
           <Link
             href={props.view}
-            className="md:w-36 my-auto w-24 h-10 hover:cursor-pointer text-center font-light text-xl p-1 border-[1px] border-white shadow-slate-500 rounded-md  hover:shadow-none shadow-[6px_6px_0px_rgb(0,0,0)]  hover:translate-y-0 -translate-y-1 hover:translate-x-0 -translate-x-1 transition-all"
+            className="md:w-36 invisible md:visible my-auto w-24 h-10 hover:cursor-pointer text-center font-light text-xl p-1 border-[1px] border-white shadow-slate-500 rounded-md  hover:shadow-none shadow-[6px_6px_0px_rgb(0,0,0)]  hover:translate-y-0 -translate-y-1 hover:translate-x-0 -translate-x-1 transition-all"
           >
             View
           </Link>
           </div>
           
         </div>
-      <div className="text-4xl font-thin ">{props.description}</div>
+      <div className="md:text-4xl text-2xl font-thin ">{props.description}</div>
+      <div className=" flex justify-center h-full items-center  ">
+          <Link
+            href={props.view}
+            className="md:invisible mt-8 visible my-auto w-36 h-10 hover:cursor-pointer text-center font-light text-xl p-1 border-[1px] border-white shadow-slate-500 rounded-md  hover:shadow-none shadow-[6px_6px_0px_rgb(0,0,0)]  hover:translate-y-0 -translate-y-1 hover:translate-x-0 -translate-x-1 transition-all"
+          >
+            View
+          </Link>
+          </div>
       </div>
     </div>
   );
