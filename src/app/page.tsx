cf74752052798251bf0b7b68e23f9873f6840500
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-08-15 09:56:14 +0530

    update : content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
           <a
             className="hover:cursor-pointer hover:text-white transition-all"
             href="https://github.com/fal3n-4ngel"
-            
+
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -61,11 +61,11 @@ export default function Home() {
             className="hover:cursor-pointer hover:text-white transition-all"
             href="https://www.linkedin.com/in/fal3n-4ngel/"
           >
-           
-            <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 448 512" fill="#ffffff"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
-            
+
+            <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 448 512" fill="#ffffff"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" /></svg>
+
           </a>
-          
+
         </div>
         <a
           href="mailto:adiadithyakrishnan@gmail.com"
@@ -188,9 +188,10 @@ export default function Home() {
             url3="/sus2.png"
             name="Sus Goals"
             description="
-            Sus Goals is simply a project for my Design and Engineering Course under BTech in CSE :<
-            Anyway this site gives you a random Sustainable Goal that refreshes daily,DO IT
-             and click the done button, it will increment the world wide counter for the goals reached. 
+            Sus Goals serves a noble purpose. Each day, you're presented with a random Sustainable Goal. 
+            Take actionable steps towards these goals and click the 'Done' button.
+            Every completed goal increments the global counter, symbolizing the 
+            collaborative impact of individuals striving for a more sustainable world 
             
             "
             type="Website | React.js , Next.js, Typescript , Tailwindcss , Firebase"
