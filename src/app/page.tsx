fff0a5e03827c7379a8df233396650c74032a7dd
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-01-01 18:09:02 +0530

    fix : responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,27 +111,39 @@ export default function Home() {
           <div className="h-[180px] w-[180px] bg-orange-500 rounded-full opacity-20 z-[-1]"></div>
         </motion.div>
 
-        <section className="flex w-full h-full min-h-screen items-center justify-center bg-[#e0e0e0] dark:bg-[#121212] transition-all duration-200 md:-mt-[100px] mt-[30px]">
+        <section className="flex w-full h-full min-h-screen items-center justify-center bg-[#e0e0e0] dark:bg-[#121212] transition-all duration-200 md:-mt-[150px] mt-[30px]">
           <div className="flex flex-col w-full font-latto  z-[10] text-center items-center justify-center ">
             <img
               src="/images/me.png"
               className="md:w-[400px] md:h-[400px] w-[200px] h-[200px]"
             />
-            <div className="md:text-[6em] text-[4rem]">
-              I&apos;m Adi <br className="flex md:hidden" /> <span className="md:flex hidden text-[2rem] font-latto">- a Versatile
-              Developer,</span>
+            <div className="md:text-[7em] text-[4rem]">
+              I&apos;m Adi <br className="flex md:hidden" />
             </div>
-            <div className="md:hidden flex  text-[2rem] font-latto">- a Versatile
-              Developer,</div>
+            <div className=" flex md:text-[5rem]  text-[2rem] font-latto">
+              - a Versatile Developer,
+            </div>
 
-            <div className="md:text-[4rem] text-[1.5rem] font-poppins opacity-60">
+            <div className="md:text-[2rem] text-[1.5rem] font-poppins opacity-60">
               and I like to code{" "}
             </div>
           </div>
         </section>
 
         <section className="min-h-screen bg-[#e0e0e0] dark:bg-[#121212] z-[1]">
-          .
+          <div className="flex w-[80%] mx-auto">
+            <div className="text-[5rem] font-latto">Portfolio</div>
+          </div>
+          <div className="relative flex w-[880px] h-[880px] mx-auto mt-10 justify-center items-center overfloe-hidden">
+            <div>
+              <div className="w-[400px] h-[400px] bg-green-300  hover:z-[10] m-[20px] rounded-[20px] hover:w-[850px] hover:h-[850px] absolute top-0  left-0 transition-all duration-300 "></div>
+              <div className="w-[400px] h-[400px] bg-blue-300 hover:z-[10] m-[20px] rounded-[20px] order-2 hover:order-1 hover:w-[850px] hover:h-[850px] absolute top-[440px] hover:top-0 left-0 transition-all duration-300"></div>
+            </div>
+            <div>
+              <div className="w-[400px] h-[400px] bg-red-300 hover:z-[10] m-[20px] rounded-[20px] order-4 absolute top-0 right-0 hover:w-[850px] hover:h-[850px] transition-all duration-300"></div>
+              <div className="w-[400px] h-[400px] bg-orange-300 hover:z-[10] m-[20px] rounded-[20px] top-[440px] hover:top-0 order-5 absolute right-0 hover:w-[850px] hover:h-[850px] transition-all duration-300"></div>
+            </div>
+          </div>
         </section>
       </motion.main>
     </main>
